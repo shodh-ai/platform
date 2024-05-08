e909c5de303481e7cf3a73d8ca96e36bbf7cd7d6
Vrishin Patel <vrishin_patel@outlook.com>
2024-05-08 16:05:58 +0530

    chore: Add checkbox to Form

--- components/Form.tsx
+++ components/Form.tsx
@@ -5,6 +5,7 @@ import type { HTMLProps, ComponentProps } from 'react';
 interface IFormSubComponents {
 	Item: typeof Item;
 	Label: typeof Label;
+	CheckBox: typeof CheckBox;
 	TextInput: typeof TextInput;
 	SubmitButton: typeof SubmitButton;
 }
@@ -19,16 +20,6 @@ const Item: FC<HTMLProps<HTMLDivElement>> = (props) => {
 	);
 };
 
-const TextInput: FC<HTMLProps<HTMLInputElement>> = (props) => {
-	return (
-		<input
-			type="text"
-			className="w-full rounded-md border border-astronaut/20 bg-astronaut/5 px-[22px] py-[18px] font-semibold text-astronaut"
-			{...props}
-		/>
-	);
-};
-
 const Label: FC<HTMLProps<HTMLLabelElement>> = (props) => {
 	const { children, ...rest } = props;
 
@@ -36,6 +27,34 @@ const Label: FC<HTMLProps<HTMLLabelElement>> = (props) => {
 		<label className="mb-[14px] text-base font-medium text-astronaut" {...rest}>
 			{children}
 		</label>
+	);
+};
+
+const CheckBox: FC<HTMLProps<HTMLInputElement>> = (props) => {
+	const { children, id, ...rest } = props;
+
+	return (
+		<div className="flex w-full gap-4 rounded-md border border-astronaut/20 bg-astronaut/5 p-[22px]">
+			<input
+				type="checkbox"
+				id={id}
+				className="peer aspect-square h-5 shrink-0 appearance-none rounded-full border border-astronaut bg-center bg-no-repeat checked:bg-[url(/checked.svg)] focus:outline-none"
+				{...rest}
+			/>
+			<label htmlFor={id} className="font-medium text-astronaut">
+				{children}
+			</label>
+		</div>
+	);
+};
+
+const TextInput: FC<HTMLProps<HTMLInputElement>> = (props) => {
+	return (
+		<input
+			type="text"
+			className="w-full rounded-md border border-astronaut/20 bg-astronaut/5 px-[22px] py-[18px] font-semibold text-astronaut"
+			{...props}
+		/>
 	);
 };
 
@@ -65,6 +84,7 @@ const Form: FC<HTMLProps<HTMLFormElement>> & IFormSubComponents = (props) => {
 
 Form.Item = Item;
 Form.Label = Label;
+Form.CheckBox = CheckBox;
 Form.TextInput = TextInput;
 Form.SubmitButton = SubmitButton;
 
