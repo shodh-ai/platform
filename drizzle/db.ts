394f012d382b1f2602e49048dcd2432ecfca8212
Vrishin Patel <vrishin_patel@outlook.com>
2024-05-24 13:19:40 +0530

    chore: Add function to retrieve job data

--- drizzle/db.ts
+++ drizzle/db.ts
@@ -1,5 +1,6 @@
 import 'drizzle/envConfig';
 
+import { eq } from 'drizzle-orm';
 import { drizzle } from 'drizzle-orm/vercel-postgres';
 
 import { sql } from '@vercel/postgres';
@@ -11,9 +12,20 @@ type User = typeof schema.JobDescriptionsTable.$inferInsert;
 export const db = drizzle(sql, { schema });
 
 export const addNewJob = async (user: User) => {
-	const result = await db.insert(schema.JobDescriptionsTable).values(user).returning({
-		id: schema.JobDescriptionsTable.id,
+	const jdTable = schema.JobDescriptionsTable;
+
+	const result = await db.insert(jdTable).values(user).returning({
+		id: jdTable.id,
 	});
 
 	return result;
+};
+
+export const getJobData = async (id: number) => {
+	const jdTable = schema.JobDescriptionsTable;
+
+	const result = await db.select().from(jdTable).where(eq(jdTable.id, id));
+
+	if (result) return result[0];
+	return null;
 };
